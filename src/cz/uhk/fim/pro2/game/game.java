be5eef1c2fa382361930cd30134c2c1cfcaa59a2
kubape1 <kubape1@J09-05.uhk.cz>
2016-10-19 16:13:47 +0200

    začátek

---
+++ src/cz/uhk/fim/pro2/game/game.java
@@ -0,0 +1,10 @@
+package cz.uhk.fim.pro2.game;
+
+public class game {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/pozdrav/App.java
+++ src/pozdrav/App.java
@@ -1,4 +1,4 @@
-package PROJEKT;
+package pozdrav;
 
 public class App {
 
